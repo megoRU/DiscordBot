fe3f1cbc6da8aac5a08b0a539f124a935dafa065
megoRU <savin.yura2014@gmail.com>
2020-09-09 00:28:38 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    JDA jda = JDABuilder.createDefault("NzUzMDAxNzQ5NjUyMTc3MDI4.X1f1hw.8UPL84pkbZakPWgbtJsGAgQCBIE").build();
+
+  }
+
+}
