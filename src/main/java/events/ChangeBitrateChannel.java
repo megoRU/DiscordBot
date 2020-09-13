5d374349a500bf74d56705385fc6e8ab91986537
megoRU <savin.yura2014@gmail.com>
2020-09-14 01:03:33 +0300

    Version 1.4.6
    1. fix

--- src/main/java/events/ChangeBitrateChannel.java
+++ src/main/java/events/ChangeBitrateChannel.java
@@ -3,6 +3,7 @@ package events;
 import java.util.Objects;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,6 +20,21 @@ public class ChangeBitrateChannel extends ListenerAdapter {
     }
     if (idUser.equals("310364711587676161")) {
       event.getNewValue().getManager().setBitrate(45000).queue();
+    }
+  }
+
+  //TODO Need test
+  @Override
+  public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
+    String idUser = Objects.requireNonNull(event.getMember()).getUser().getId();
+    User user = event.getChannelLeft().getJDA().getUserById("310364711587676161");
+    boolean memberInVoiceChannel = event.getChannelJoined().getGuild().getMember(user).getVoiceState().inVoiceChannel();
+
+    if (!idUser.equals("310364711587676161") & !memberInVoiceChannel) {
+      event.getNewValue().getManager().setBitrate(45000).queue();
+    }
+    if (idUser.equals("310364711587676161")) {
+      event.getNewValue().getManager().setBitrate(96000).queue();
     }
   }
 }
