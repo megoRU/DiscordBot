74dd8e4dc83b54cc6a090486f3ee8e24515cf37a
megoRU <savin.yura2014@gmail.com>
2020-11-03 22:19:56 +0300

    Version 2.2.5
    1. Add RepeatMyMessage

---
+++ src/main/java/messagesevents/RepeatMyMessage.java
@@ -0,0 +1,38 @@
+package messagesevents;
+
+import java.util.List;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class RepeatMyMessage extends ListenerAdapter {
+
+  public final String REPEAT = "!repeat";
+  public final String REPEAT2 = "repeat";
+  public final String REPEAT_RU = "повтори";
+  public final String REPEAT_RU2 = "!повтори";
+
+  @Override
+  public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+    String message = event.getMessage().getContentRaw().toLowerCase().trim();
+    String[] split = message.split(" ", 2);
+
+    if (split[0].matches(REPEAT)
+        || split[0].matches(REPEAT2)
+        || split[0].matches(REPEAT_RU)
+        || split[0].matches(REPEAT_RU2)) {
+      event.getChannel().sendMessage("✅").queue();
+
+      try {
+      Thread.sleep(250);
+      List<Message> messages = event.getChannel().getHistory().retrievePast(2).complete();
+
+      event.getChannel().deleteMessages(messages).queue();
+      event.getChannel().sendMessage(split[1]).queue();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
--- src/main/java/startbot/BotStart.java
+++ src/main/java/startbot/BotStart.java
@@ -18,6 +18,7 @@ import messagesevents.MessageInfoHelp;
 import messagesevents.MessageMoveUser;
 import messagesevents.MessagePing;
 import messagesevents.MessageUptimeBot;
+import messagesevents.RepeatMyMessage;
 import messagesevents.YoutubeUrlWithTime;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -53,6 +54,7 @@ public class BotStart {
     jdaBuilder.addEventListeners(new EventJoinMemberToGuildSetRole());
     jdaBuilder.addEventListeners(new CountConnectionsEvent());
     jdaBuilder.addEventListeners(new LogWhoEnterLeaveMoveChannel());
+    jdaBuilder.addEventListeners(new RepeatMyMessage());
     jda = jdaBuilder.build();
     jda.awaitReady();
   }
