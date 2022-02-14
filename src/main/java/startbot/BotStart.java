ac9f9a32709d54be416f9c687d93d6320496fff6
megoRU <savin.yura2014@gmail.com>
2022-02-14 16:10:06 +0300

    Version: 5.0.1 1. Fix build

--- src/main/java/startbot/BotStart.java
+++ src/main/java/startbot/BotStart.java
@@ -29,28 +29,17 @@ public class BotStart {
         jdaBuilder.setActivity(Activity.playing("!help"));
         jdaBuilder.setBulkDeleteSplittingEnabled(false);
         jdaBuilder.addEventListeners(new MessagePing());
-        jdaBuilder.addEventListeners(new MessageDeleting());
         jdaBuilder.addEventListeners(new MessageInfoHelp());
-        jdaBuilder.addEventListeners(new MessageMoveUser());
-        jdaBuilder.addEventListeners(new MessageUptimeBot());
         jdaBuilder.addEventListeners(new MessageWhenBotLeaveJoinToGuild());
         jdaBuilder.addEventListeners(new MessageWhoEnterLeaveChannel());
         jdaBuilder.addEventListeners(new YoutubeUrlWithTime());
         jdaBuilder.addEventListeners(new ChangeBitrateChannel());
         jdaBuilder.addEventListeners(new GoogleSearch());
-        jdaBuilder.addEventListeners(new ExchangeRates());
-        jdaBuilder.addEventListeners(new FlipCoin());
-        jdaBuilder.addEventListeners(new ExchangeValue());
         jdaBuilder.addEventListeners(new UserJoinToGuild());
         jdaBuilder.addEventListeners(new CountConnectionsEvent());
         jdaBuilder.addEventListeners(new LogWhoEnterLeaveMoveChannel());
-        jdaBuilder.addEventListeners(new RepeatMyMessage());
-        jdaBuilder.addEventListeners(new SendingMessagesToGuilds());
         jdaBuilder.addEventListeners(new GameOfDice());
         jdaBuilder.addEventListeners(new MessageChangeBitrate());
-        jdaBuilder.addEventListeners(new MessageKick());
-        jdaBuilder.addEventListeners(new MessageBan());
-        jdaBuilder.addEventListeners(new MessagePoll());
         jdaBuilder.addEventListeners(new PrefixChange());
         jdaBuilder.addEventListeners(new MessageCheckBotSettings());
 
